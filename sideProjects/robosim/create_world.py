124bb2c1d56ed3a5a1b972b34c8a9c0956898d01
stefan96larsen <stefan96larsen@yahoo.dk>
2021-09-23 09:42:00 +0200

    Can save and load robot path

--- sideProjects/robosim/create_world.py
+++ sideProjects/robosim/create_world.py
@@ -1,16 +1,21 @@
 import numpy as np
-import matplotlib as plt
 import cv2
 import matplotlib.pyplot as plt
+from numpy.core.numeric import full
+
 
 # local packages
-from lib.utility import do_rom_splines, calculate_angles
+from lib.utility import *
 
 class world:
-    def __init__(self, background: str, landmarks: str):
+    def __init__(self, background: str, landmarks: str, save_path: bool = False, load_path: bool = False, path_name: str = 'Aarhus_path1.json'):
        
+        self.load_path = load_path
+        self.save_path = save_path
+        self.path_name = path_name
+        
+       # Loading map from GIS data as a binary image
         img = cv2.imread(background, cv2.IMREAD_GRAYSCALE)
-
         img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
         #self.background = cv2.threshold(img, 127, 1, cv2.THRESH_BINARY)[1]
 
@@ -20,54 +25,88 @@ class world:
         img = cv2.bitwise_and(img, mask)
         self.crop_img = img[56:429, 78:578]
 
+        if self.load_path:
+            self.loaded_route = load_from_json('./sideProjects/robosim/data/robopath/'+self.path_name)
+            self.x_odo, self.y_odo, self.th_odo = zip(*np.asarray_chkfinite(self.loaded_route))
+            
+            
+        # Loading landmarks that was saved using GIS data
         with open(landmarks, 'r') as f:
             self.landmarks = np.genfromtxt(f, delimiter=',')
 
-        # cv2.imshow('map', img)
-        # cv2.imshow('cropped', self.crop_img)
 
     def draw_points(self, event, x, y, flags, param):
-        """ Draws circle when using the mouse on the image """
+        """ Draws circle when clicking (left click) on the image """
 
-        if event == cv2.EVENT_LBUTTONDBLCLK:
+        if event == cv2.EVENT_LBUTTONDOWN:
 
             cv2.circle(self.grid_copy, (x,y), 5, (0, 255, 0), -1)
             cv2.imshow('Robot_route', self.grid_copy)
             self.route.append([x,y])
 
-    def make_robot_path(self, print_coordinates: bool = True):
+    @staticmethod
+    def robot_heading(x, y, theta, length: float = 5, width: float = 0.05):
+        """
+        Method that plots the heading of every pose
+        """
+        x = x[1:-1]
+        y = y[1:-1]
+        theta = theta[1:-1]
+
+        terminus_x = x + length * np.cos(theta)
+        terminus_y = y + length * np.sin(theta)
+        plt.plot([x, terminus_x], [y, terminus_y])
+
+
+    def make_robot_path(self, plot_route: bool = True):
         self.grid = np.asarray(self.crop_img)
         self.grid_copy = self.grid.copy()
         self.route = []
 
-        cv2.namedWindow('Robot_route')
-        cv2.imshow('Robot_route',self.grid_copy)
-        cv2.setMouseCallback('Robot_route', self.draw_points, self.grid_copy)
+        # If we do not have a robot path saved in 'sideProjects/robosim/data'
+        if self.save_path:
+            cv2.namedWindow('Robot_route')
+            cv2.imshow('Robot_route',self.grid_copy)
+            cv2.setMouseCallback('Robot_route', self.draw_points, self.grid_copy)
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        # print(self.route)
-        angles = calculate_angles(self.route)
+
+            # calculating angles between all points and concatenating
+            angles = calculate_angles(self.route)
+            poses = [[pose[0], pose[1], theta] for pose, theta in zip(self.route, angles)]
+            poses = [poses[0]] + poses + [poses[-1]]
+
+            # Smoothening of route using splines
+            full_route = do_rom_splines(poses)
+            self.x_odo, self.y_odo, self.th_odo = zip(*full_route)
+
+            # Saving the reduced route to json file 
+            json_path = reduce_dimensions(np.array([self.x_odo, self.y_odo, self.th_odo]))
+            json_path1 = json_path.tolist()
+            save_to_json(json_path1,'./sideProjects/robosim/data/robopath/'+self.path_name)
         
-        poses = [[pose[0], pose[1], theta] for pose, theta in zip(self.route, angles)]
-        poses = [poses[0]] + poses + [poses[-1]]
-        
-        full_route = do_rom_splines(poses)
 
-        
-        x, y, th = zip(*full_route)
-        plt.plot(x, y)
+        # Odometry drift
+        self.x_odo_noisy, self.y_odo_noisy, self.th_odo_noisy = odometry_drift_simple(self.x_odo, self.y_odo, self.th_odo)
+        if plot_route:
+            
+            plt.plot(self.x_odo, self.y_odo, label='Original route')
+            plt.plot(self.x_odo_noisy, self.y_odo_noisy, label='Noise route')
 
-        px, py, pth = zip(*poses)
-        plt.plot(px, py, 'or')
-        
-        plt.imshow(self.crop_img, cmap='gray')
-        plt.show()
-
-        # if print_coordinates:
-        #     print(full_route)
-
+            if False:
+                px, py, pth = zip(*poses)
+                px = np.asarray_chkfinite(px)
+                py = np.asarray_chkfinite(py)
+                pth = np.asarray_chkfinite(pth)
+                
+                self.robot_heading(px, py, pth)
+            
+            
+            plt.imshow(self.crop_img, cmap='gray')
+            plt.legend(loc="upper left")
+            plt.show()
 
 
 
@@ -75,14 +114,10 @@ def main():
     # print(cv2.__version__)
     map = 'sideProjects/GIS_Extraction/plots/GIS_map3.png'
     landmarks_file = 'sideProjects/GIS_Extraction/landmarks/landmarks_points.csv'
-    show = world(map, landmarks_file)
+
+    show = world(map, landmarks_file, save_path=True, load_path=False)
     show.make_robot_path()
-    # while True:
-    #         k = cv2.waitKey(0) & 0xFF
-    #         print(k)
-    #         if k == 27:
-    #             cv2.destroyAllWindows()
-    #             break
+  
 
 if __name__ == "__main__":
     main()
--- sideProjects/robosim/lib/utility.py
+++ sideProjects/robosim/lib/utility.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from math import pow, ceil, atan2, pi
+from math import pow, atan2, pi, sqrt, cos, sin
+import json
 
 def do_rom_splines(route):
     size = len(route)
@@ -9,7 +10,10 @@ def do_rom_splines(route):
 
     for i in range(size - 3):
         c = rom_spline(route[i], route[i+1], route[i+2], route[i+3])
-        C.extend(c)
+
+        # Ingoring NaN values
+        if ~(np.isnan(np.sum(c))):
+            C.extend(c)
     
     return C
 
@@ -43,36 +47,149 @@ def traj(Pi, Pj, t, alpha):
 
     return (pow((xj - xi),2) + pow((yj-yi),2) + pow((thj - thi),2))**alpha + t
 
-# Old version    
-def do_splines(route, angles):
-        
-    poses = [(pose[0], pose[1], theta) for pose, theta in zip(route, angles)]
-    poses = [poses[0]] + poses + [poses[-1]]
-
-    full_route = [rom_spline(poses[i:i+4]) for i in range(len(poses) - 3)]
-
-    return poses, full_route
-
 
 def calculate_angles(route):
-
+    # Calculating angle between two points
     angles = [atan2((y2 - y1), (x2 - x1)) for (x1, y1), (x2, y2) in zip(route[:-1], route[1:])]
-    angles = [angles[0]] + angles
-    angles = [angle % (2 * pi) for angle in angles]
 
+    # Iterating over all angles and normalizing angles to [-pi, pi]
     for i in range(len(angles) - 1):
-        angles[i + 1] = min_theta(angles[i], angles[i + 1])
+        angles[i] = min_theta(angles[i])
     
     return angles
 
 
-def min_theta(x, y):
+def min_theta(angle):
+    # Setting minimum angular difference 
+    if (angle > pi):
+        angle -= 2 * pi
+    elif (angle <= -pi): 
+        angle += 2 * pi
 
-    if 0 <= (x and y) <= 2*pi:
-        a = (x - y) % 2*pi
-        b = (y - x) % 2*pi
-    else:
-        print('Non normalized input')
+    return angle
 
-    return -a if a < b else b
+
+def odometry_drift_simple(x, y, th, drift_constant_std: float = 0.05):
+    """ 
+    Simply adding a constant to the wheels to simulate drift 
+    The drift is computed using a normal gaussian distribution with standard deviation from the constant drift_constant_std
+
+    return:
+        The fully drifted route 
+    """
+
+    x = np.asarray_chkfinite(x)
+    y = np.asarray_chkfinite(y)
+    th = np.asarray_chkfinite(th)
+
+    x_noise = []
+    y_noise = []
+    th_noise = []
+
+    noise = 0
+    for i in range(len(x)):
+
+        x_new = x[i] + noise
+        y_new = y[i] + noise*0.1
+        th_new = th[i] + noise*0.2
+
+        x_noise.append(x_new)
+        y_noise.append(y_new)
+        th_noise.append(th_new)
+
+        noise += np.random.normal(0.1, drift_constant_std)
+
+    return x_noise, y_noise, th_noise
+    
+
+def reduce_dimensions(route):
+    """ Reducing the size of splined route down to a third of the size
+
+    Args:
+        route ([np.array([x, y, theta])]): [the full robot route]
+
+    Returns:
+        [np.array]: [reduced robot path np.array([x, y, theta])]
+    """    
+
+    reduced = []
+
+    for i in range(len(route[0,:])):
+        if (i % 1.5 == 1):
+            reduced.append(route[:,i])
+
+    print('Reduced size of path from {} to {}'.format(len(route[1]), np.shape(reduced)[0]))
+
+    return np.asarray_chkfinite(reduced)
+ 
+
+
+def save_to_json(l, name):
+    with open(name, 'w') as f:
+        json.dump(l, f)
+
+def load_from_json(name):
+    with open(name, 'r') as f:
+        return json.load(f)
+
+
+
+#######################
+# BELOW IS DEPRECATED #
+
+def odometry_drift(x, y, th, std: float = 0.3):
+    """
+    Adding gaussian noise to odometry
+    """
+    error_x = np.random.normal(1, std)
+    error_y = np.random.normal(1, std*0.5)
+    error_th = np.random.normal(1, std*0.1)
+
+    x = np.asarray_chkfinite(x)
+    y = np.asarray_chkfinite(y)
+    th = np.asarray_chkfinite(th)
+
+    dist = []
+    angle = []
+
+    # Converting to polar coordinates and back again in order to add radial noise
+    for i in range(len(x)):
+        r = sqrt(pow(x[i],2) + pow(y[i],2)) # range
+        a = atan2(x[i],y[i])    # angle
+        
+        dist.append(r * error_x)
+        angle.append(a * error_y)
+    
+    x_noise = []
+    y_noise = []
+    th_noise = []
+    
+    for i in range(len(dist)):
+        x1 = dist[i] * cos(angle[i])
+        y1 = dist[i] * sin(angle[i])
+        x_noise.append(x1)
+        y_noise.append(y1)
+        th_noise.append(th[i] * error_th)
+
+    return x_noise, y_noise, th_noise
+    
+
+def relative_position(x, y, th):
+    # Route should contain x, y, theta in cartesian absolute coordinates
+    # Returns robot path in relative coordinates
+
+    x_start = x[0]
+    y_start = y[0]
+
+    x_relative = []
+    y_relative = []
+    for i in len(x):
+
+        x_relative.append(x_start - x[i])
+        y_relative.append(y_start - y[i])
+
+    x_relative, y_relative = x_relative * np.cos(-th) - y_relative * np.sin(-th), \
+                             x_relative * np.sin(-th) + y_relative * np.cos(-th)
+
+    return x_relative, y_relative
 
