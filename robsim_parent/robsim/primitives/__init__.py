28c6c4eff14dde61ac48ec50b62c15d9f61e476e
Stefan Larsen <46599903+stefan96larsen@users.noreply.github.com>
2021-11-01 10:15:06 +0100

    Delete robsim_parent directory

--- robsim_parent/robsim/primitives/__init__.py
+++
@@ -1,3 +0,0 @@
-from robsim.primitives.line import Line
-from robsim.primitives.point import Point
-from robsim.primitives.pose import Pose
--- robsim_parent/testfile/datapane.py
+++
@@ -1,4 +0,0 @@
-
-import datapane as dp
-
-dp.login(token='a77e0014a1c663e4e1808baf397946c2451898e5')
