7a1ffbc350a0e5228c792ee5d5a8183390cbf08d
MJensen1231992 <mjensen1231992@gmail.com>
2022-01-19 13:50:21 +0100

    her kommer run_slam

--- graphSLAM/run_slam.py
+++ graphSLAM/run_slam.py
@@ -1,58 +1,49 @@
 import warnings
 import copy
-
+import sys
 from utils.linearize import *
 from utils.helper import *
 from utils.error import *
 from utils.graph_plot import *
 from utils.g2o_loader import load_g2o_graph
 from utils.slam_iterate import *
-import sys
+
 sys.path.append
 warnings.filterwarnings('ignore')
 
 
-
 class Graph: 
-    def __init__(self, x, nodes, edges,lut, nodeTypes, withoutBearing=False):
+    def __init__(self, x, nodes, edges,lut, nodeTypes, descriptor):
         self.x = x
         self.nodes = nodes
         self.edges = edges
         self.lut = lut
-        self.withoutBearing = withoutBearing
         self.nodeTypes = nodeTypes
+        self.descriptor = descriptor 
 
 class G2O:
-    def __init__(self, filename, gt):
+    def __init__(self, filename, gt,):
         
-        self.graph = load_g2o_graph(filename,gt, noBearing=False)
+        self.graph = load_g2o_graph(filename,gt,descriptor=False)
         self.error = compute_global_error(self.graph)
-        
-
 
 if __name__ == '__main__' :
 
-    noise = G2O('graphSLAM/data/noise_20220117-125333.g2o', gt=False)
+    noise = G2O('graphSLAM/data/external/M3500.g2o', gt=False)
     n_graph = noise.graph
     
-    
-    ground = G2O('graphSLAM/data/ground_truth_20220117-125333.g2o', gt=True)
+    ground = G2O('graphSLAM/data/ground_truth_20220119-104232.g2o', gt=True)
     g_graph = ground.graph
 
     pre_graph = copy.deepcopy(n_graph)
     # poses_per_landmark(n_graph)
     # statistics_plot(n_graph)
     # landmark_ba(n_graph,g_graph,pre_graph)
-    # print(n_graph.nodes.items())
-    # plot_ground_together_noise(n_graph,g_graph,pre_graph)
+    plot_ground_together_noise(n_graph,g_graph,pre_graph)
     # plot_map(n_graph,g_graph)
-
-    # _, Hs , H, _ = linearize_solve(n_graph)
-    # plt.spy(Hs)
-    # plt.show()
-    # hnul = np.nonzero(H)
-    # print(np.shape(hnul))
-    dx = graph_slam_run_algorithm(n_graph,30, g_graph, pre_graph)
+    _,_,_, dcs_array = linearize_solve(n_graph)
+    dcs_arrayplot(dcs_array)
+    dx = graph_slam_run_algorithm(n_graph, 30, g_graph, pre_graph)
 
 
     
